6cc7bc8204d733c2ab998033b77374daab8b25b9
d-bryan <dylan.g.bryan@gmail.com>
2020-01-04 11:31:59 -0500

    initial upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# UA Ecommerce Analytics Software Engineer
+
+## Take Home Assessment
+
+### Summary
+
+The following coding exercise is used so we can assess your development skills. Remember,
+this is your chance to show us your chops!
+
+This coding exercise is designed as an open approach problem. The task we are asking you to
+complete is a guideline to get you started. We expect you to complete the exercise as exactly
+as possible and use best practices and methods while meeting the requirements guidelines.
+
+### Project Details
+
+Create an HTML document using CSS and Vanilla Javascript to mimic the HTML document in
+the attached video.
+
+Acceptance Criteria:
+
+- As a user, I would like to be able to interact with two buttons. The first button should be
+used to create rows saying hello. The second button should be used to create rows
+saying goodbye.
+- Please follow the video for exactness on functionality and design
+Requirements.
+- Must be cross browser compatible for latest Chrome, IE 10 & 11, Edge, latest Safari.
+- Please complete task with ONE Javascript event listener.
+- Design should be flexible for both mobile and desktop layouts.
+- Submit your source code via GitHub by the requested due date.
+- Invite the hiring manager and expect that we will clone the repository to make it
+accessible to the interview group.
---
+++ js/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// constants and selectors for use in document
+const fillContainer = document.getElementById("element--container");
+const buttonContainer = document.getElementById("button-container");
+const paragraph = document.createElement("p");
+const dataAttribute = fillContainer.getAttribute("data-number-of-elements");
+const numElements = parseInt(dataAttribute,10);
+
+/**
+ * Creates and fills new elements with argument passed through
+ * @param {Text} data - parameter to fill the paragraph tags with
+ */
+function createParagraphElements (data) {
+  // loop to create paragraph elements
+  for (let i = 0; i < numElements; i ++) {
+    let newParagraph = document.createElement("p");
+    fillContainer.append(newParagraph);
+    newParagraph.innerHTML = data;
+  }
+}
+
+/**
+ * On click event that changes the inner elements of "element-container" div
+ * @param {Click} event - click event
+ */
+function createElements(event) {
+
+  // if event target is hello
+  if (event.target.id === "hello") {
+    
+    // while there are children elements delete them before adding the new ones
+    while (fillContainer.firstChild) {
+      fillContainer.removeChild(fillContainer.firstChild);
+    }
+    // create the paragraph elements
+    createParagraphElements(event.target.id);
+
+    // if event target is goodbye
+  } else if (event.target.id === "goodbye") {
+
+    // while there are children elements delete them before adding the new ones
+    while (fillContainer.firstChild) {
+      fillContainer.removeChild(fillContainer.firstChild);
+    }
+    // create the paragraph elements
+    createParagraphElements(event.target.id);
+
+  } else {
+    // if both have failed log an error to the console with the event
+    console.error("There was an error with the event listener.", event);
+  }
+
+}
+
+// single event listener for both buttons
+buttonContainer.addEventListener("click", createElements, false);
+
